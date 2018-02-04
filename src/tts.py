9c1118a58e9a7fe71310251338a507fe22026704
qqii <Qi-rui.Chen.2015@my.bristol.ac.uk>
2018-02-04 12:06:23 +0000

    Add more phrases

--- src/tts.py
+++ src/tts.py
@@ -2,10 +2,10 @@
 
 from gtts import gTTS
 
-shake_phrases = ["Stop shaking me", "Put me down", "This hurts", "Why are you doing this to me?"]
-push_phrases = ["Stop pushing me", "Leave me alone", "Get off me", ]
-light_phrases = ["The light, it burns", "I don't like the outside", "Leave me be please"]
-sad_phrases = ["Why did you leave me alone?","Please come back", "I'm so sad by myself"]
+shake_phrases = ["Stop shaking me", "Put me down", "This hurts", "Why are you doing this to me?", "Why are you being so voilent?", "I beg you, please stop!"]
+push_phrases = ["Stop pushing me", "Leave me alone", "Get off me", "I don't appreciate being pushed around", "That hurts", "Ouch! Why did you do that?"]
+light_phrases = ["The light, it burns", "I don't like the outside", "Leave me be please", "Stop looking at my insides.", "Let me be alone in the dark, in pease."]
+sad_phrases = ["Why did you leave me alone?","Please come back", "I'm so sad by myself", "Why does everyone leave me?", "I'm so alone."]
 
 
 for shake in shake_phrases:
